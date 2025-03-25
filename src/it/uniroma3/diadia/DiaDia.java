64acd39ce22a653985aa62323b6dc04619e4bfef
Mizzio28 <maurizio.pecorella@icloud.com>
2025-03-25 17:18:36 +0100

    comando prendi posa
    posa prendi

--- src/it/uniroma3/diadia/DiaDia.java
+++ src/it/uniroma3/diadia/DiaDia.java
@@ -33,7 +33,7 @@ public class DiaDia {
 			"o regalarli se pensi che possano ingraziarti qualcuno.\n\n"+
 			"Per conoscere le istruzioni usa il comando 'aiuto'.";
 	
-	static final private String[] elencoComandi = {"vai", "aiuto", "fine"};
+	static final private String[] elencoComandi = {"vai", "aiuto", "fine", "posa", "prendi"};
 
 	private Partita partita;
 	private IOConsole io;
